7ebfbf712d177ee5d145cb54694c771cf89439ec
Yerong Li <yerong.li@outlook.com>
2021-11-08 10:36:31 -0600

    Add fine-grained bins

--- plot/duration.py
+++ plot/duration.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import random
-bins = np.linspace(0, 180, 100)
+bins = np.linspace(0, 180, 20)
 x = [None, None, None]
 df = pd.read_csv('../videos_dataset.csv')
 cutoff_likes95 = np.percentile(df.likes.tolist(), 95)
