ee8aeeabd8f43371d0a00065f616b698c2f3f4c1
Yerong Li <yerong.li@outlook.com>
2021-11-08 08:20:40 -0600

    Count blanks

--- plot/hashtag.py
+++ plot/hashtag.py
@@ -7,16 +7,14 @@ df = pd.read_csv('../videos_dataset.csv')
 cutoff_likes = np.percentile(df.likes.values.tolist(), 75)
 # print(cutoff_likes)
 most_liked_df = df[df.likes >= int(cutoff_likes)]
-print(most_liked_df.columns)
-print(most_liked_df.shape[0])
 blank_count = [eval(hashtags) for hashtags in most_liked_df.hashtag.values.tolist()]
 blank_count = [1 for hashtags in blank_count if len(hashtags)==0]
 
 # print(most_liked_df.hashtag.values.tolist())
-print(len(blank_count))
-labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
+freq_blank = len(blank_count)/ most_liked_df.shape[0] * 100
+labels = 'HashTags', 'No HashTags'
 
-sizes = [15, 30, 45, 10]
+sizes = [freq_blank, 100 - freq_blank]
 explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
 fig1, ax1 = plt.subplots()
 ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
