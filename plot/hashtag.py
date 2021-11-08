aaa9c5376e37939ca7a1725b523782685b818fbc
Yerong Li <yerong.li@outlook.com>
2021-11-08 07:59:49 -0600

    print pwd

--- plot/hashtag.py
+++ plot/hashtag.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
 df = pd.read_csv('../videos_dataset.csv')
 cutoff_likes = np.percentile(df.likes.values.tolist(), 90)
 # print(cutoff_likes)
 most_liked_df = df[df.likes >= int(cutoff_likes)]
+blank_count = most_liked_df.has
 labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
 
 sizes = [15, 30, 45, 10]
@@ -16,3 +18,5 @@ ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
 ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
 plt.show()
+cwd = os.getcwd()
+print(cwd)
