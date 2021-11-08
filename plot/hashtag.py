66c60601dc03f6fa9be6031ecdec454f27c8ffe1
Yerong Li <yerong.li@outlook.com>
2021-11-08 08:35:38 -0600

    two axes

--- plot/hashtag.py
+++ plot/hashtag.py
@@ -19,10 +19,10 @@ sizes = [100-freq_blank, freq_blank]
 
 # sizes = [15, 30, 45, 10]
 explode = (0, 0.1)  # only "explode" the 2nd slice (i.e. 'Hogs')
-fig1, ax1 = plt.subplots()
-ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+fig1, ax = plt.subplots(1,2)
+ax[0].pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
         shadow=True, startangle=90)
-ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+ax[0].axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
 filename = 'hashtag.png'
 plt.savefig(filename)
