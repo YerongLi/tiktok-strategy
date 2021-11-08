c77f67cf7f813c4c0ec6fae395b89735553d7f44
Yerong Li <yerong.li@outlook.com>
2021-11-08 10:02:58 -0600

    Transparent

--- plot/duration.py
+++ plot/duration.py
@@ -13,14 +13,14 @@ x[0] = most_liked_df.videoduration.values.tolist()
 # labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
 # pyplot.hist(x, bins, alpha=0.5, label='x')
 # pyplot.hist(y, bins, alpha=0.5, label='y')
-bins = np.linspace(-10, 10, 100)
+bins = np.linspace(-10, 10, 10000)
 cutoff_likes75 = np.percentile(df.likes.values.tolist(), 75)
 # print(cutoff_likes)
 most_liked_df = df[df.likes >= int(cutoff_likes75)]
 most_liked_df = most_liked_df[most_liked_df.likes < int(cutoff_likes95)]
-x[0] = [random.gauss(3,1) for _ in range(400)]
-x[1] = [random.gauss(4,2) for _ in range(400)]
-# x[1] = most_liked_df.videoduration.values.tolist()
+# x[0] = [random.gauss(3,1) for _ in range(400)]
+# x[1] = [random.gauss(4,2) for _ in range(400)]
+x[1] = most_liked_df.videoduration.values.tolist()
 
 fig1, ax = plt.subplots(2,1)
 ax[0].hist(x[0], bins, alpha=0.3, label='95')
