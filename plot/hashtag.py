7226d7deb0f0d98ae356daf6cc7a71ee51f3ac49
Yerong Li <yerong.li@outlook.com>
2021-11-08 08:12:50 -0600

    Remove printing

--- plot/hashtag.py
+++ plot/hashtag.py
@@ -10,7 +10,7 @@ most_liked_df = df[df.likes >= int(cutoff_likes)]
 print(most_liked_df.columns)
 print(most_liked_df.shape[0])
 blank_count = [1 for hashtags in most_liked_df.hashtag.values.tolist() if len(eval(hashtags))==1]
-print(most_liked_df.hashtag.values.tolist())
+# print(most_liked_df.hashtag.values.tolist())
 print(len(blank_count))
 labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
 
