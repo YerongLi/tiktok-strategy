38fd0a5d0572e4439dbf15abd6c2767babfc3bca
yerong <yerong.li@outlook.com>
2021-12-02 21:50:08 -0600

    link file create

--- graph.py
+++ graph.py
@@ -8,3 +8,7 @@ with open('tiktok_data/node.dat', 'w') as f:
         f.write(f'{author} a\n')
     for video in videos:
         f.write(f'{video} v\n')
+
+with open('tiktok_data/link.dat', 'w') as f:
+    for i in range(df.shape[0]):
+        f.write(f"{df.iloc[i]['author.uniqueId']} {df.iloc[i].id}\n")
