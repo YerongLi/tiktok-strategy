90df91e8630db7a12be0d73d7bd79ad22fececf2
yerong <yerong.li@outlook.com>
2021-12-02 19:44:15 -0600

    Generate node data

---
+++ graph.py
@@ -0,0 +1,7 @@
+import pandas as pd
+df = pd.read_csv('videos_dataset.csv')
+authors = sorted(set(df.authors.values.tolist()))
+
+with open('tiktok_dat/node.dat')
+    for author in authors:
+        f.write(f'{author} a\n')
