847a0ea41b7824348f7cb4a16c2e0a884ae8d286
yerong <yerong.li@outlook.com>
2021-12-03 00:58:19 -0600

    multiprocessing

--- generate_dataset.py
+++ generate_dataset.py
@@ -62,7 +62,8 @@ content = []
 with open(f'{args.file}') as f:
     lines = f.readlines()
     verified_user = set([eval(line)['id'] for line in lines])
-
+# print()
+verified_user = [6947197656385799174]
 for entry in tqdm.tqdm(list(videos_collection.find({'author': {'id': {'$in': list(verified_user)}}}))):
     dic = simple_dict(dict(entry))
     content.append([dic[k] for k in headers])
