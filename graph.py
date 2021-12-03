a867f2387669b0a5d6e4a926b94e5736fd7147c9
yerong <yerong.li@outlook.com>
2021-12-02 21:24:08 -0600

    node file

--- graph.py
+++ graph.py
@@ -1,7 +1,7 @@
 import pandas as pd
 df = pd.read_csv('videos_dataset.csv')
 authors = sorted(set(df['author.uniqueId'].values.tolist()))
-videos = sorted(set(df.video.id.tolist()))
+videos = sorted(set(df.values.tolist()))
 
 with open('tiktok_dat/node.dat', 'w') as f:
     for author in authors:
