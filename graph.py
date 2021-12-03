075db868351bc623eab894193c62141b5dc9c818
yerong <yerong.li@outlook.com>
2021-12-02 22:56:46 -0600

    Generate duetFromId in the dataset

--- graph.py
+++ graph.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import logging
+import tqdm
 
 df = pd.read_csv('videos_dataset.csv')
 a = set(df.id.values.tolist())
@@ -18,7 +19,7 @@ with open('tiktok_data/node.dat', 'w') as f:
         f.write(f'{music} m\n')
 
 with open('tiktok_data/link.dat', 'w') as f:
-    for i in range(df.shape[0]):
+    for i in tqdm.tqdm(range(df.shape[0])):
         f.write(f"{df.iloc[i]['author.uniqueId']} {df.iloc[i].id}\n")
         f.write(f"{df.iloc[i].id} {df.iloc[i]['author.uniqueId']}\n")
         f.write(f"{df.iloc[i].id} {df.iloc[i]['music.title']}\n")
