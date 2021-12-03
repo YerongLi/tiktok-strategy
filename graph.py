02cb2067157a5829e1b3fcf57da54fe06d612f05
yerong <yerong.li@outlook.com>
2021-12-03 01:34:02 -0600

    debug:skip orignal sound

--- graph.py
+++ graph.py
@@ -11,19 +11,19 @@ logging.basicConfig(level=logging.DEBUG)
 logging.info(f'There are {len(a)} vidoes and {len(b)} dueted videos : {len(a.union(b))} videos in total.')
 
 with open('tiktok_data/node.dat', 'w') as f:
-    for author in sorted(set(df['author.uniqueId'].values.tolist())):
+    for author in sorted(set(df['author.id'].values.tolist())):
         f.write(f'{author} a\n')
     for video in sorted(a.union(b)):
         f.write(f'{video} v\n')
     for music in sorted(set(df['music.title'].values.astype(str).tolist()), key=len):
         if music != 'original sound':
-            f.write(f'{music} m\n')
+            f.write(f'{music.replace(' ', '==')} m\n')
 
 with open('tiktok_data/link.dat', 'w') as f:
     for i in tqdm.tqdm(range(df.shape[0])):
-        f.write(f"{df.iloc[i]['author.uniqueId']} {df.iloc[i].id}\n")
+        f.write(f"{df.iloc[i]['author.id']} {df.iloc[i].id}\n")
         f.write(f"{df.iloc[i].id} {df.iloc[i]['author.uniqueId']}\n")
         if df.iloc[i]['music.title'] != 'original sound':
-            f.write(f"{df.iloc[i]['music.title']} {df.iloc[i].id}\n")
+            f.write(f"{df.iloc[i]['music.title'].replace(' ', '==')} {df.iloc[i].id}\n")
         if df.iloc[i].duetFromId != 0:
             f.write(f"{df.iloc[i].duetFromId} {df.iloc[i].id}\n")
