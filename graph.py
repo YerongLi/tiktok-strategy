65bd4035c921413b3b115e85254d227a8b622e83
yerong <yerong.li@outlook.com>
2021-12-02 22:51:42 -0600

    Generate duetFromId in the dataset

--- graph.py
+++ graph.py
@@ -1,11 +1,13 @@
 import pandas as pd
+import logging
+
 df = pd.read_csv('videos_dataset.csv')
-a = list(set(df.id.values.tolist()))
-b = list(set(df.duetFromId.values.tolist()))
-print(len(a))
-print(len(b))
-print(len(set(a).union(set(b))))
-print(a[0])
+a = set(df.id.values.tolist()))
+b = set(df.duetFromId.values.tolist()))
+
+logging.basicConfig(level=logging.DEBUG)
+logging.debug(f'There are {len(a)} vidoes and {len(b)} dueted videos : {len(len(a.union(b)))} videos in total.')
+
 for i in b:
     if i != 0:
         print(i)
@@ -13,7 +15,7 @@ for i in b:
 with open('tiktok_data/node.dat', 'w') as f:
     for author in sorted(set(df['author.uniqueId'].values.tolist())):
         f.write(f'{author} a\n')
-    for video in sorted(set(df.id.values.tolist())):
+    for video in sorted(set(df.id.values.tolist()).union(set(df.duetFromId.values.tolist()))):
         f.write(f'{video} v\n')
     for music in sorted(set(df['music.title'].values.astype(str).tolist()), key=len):
         f.write(f'{music} m\n')
@@ -23,6 +25,4 @@ with open('tiktok_data/link.dat', 'w') as f:
         f.write(f"{df.iloc[i]['author.uniqueId']} {df.iloc[i].id}\n")
         f.write(f"{df.iloc[i].id} {df.iloc[i]['author.uniqueId']}\n")
         f.write(f"{df.iloc[i].id} {df.iloc[i]['music.title']}\n")
-
-
-# with open()
+        F.write(f"{df.iloc[i].id}")
