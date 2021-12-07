d2b263454b3bca1f88e98f45169a927848ad2a0f
yerong <yerong.li@outlook.com>
2021-12-06 18:25:23 -0600

    count duet videos

--- generate_dataset.py
+++ generate_dataset.py
@@ -125,8 +125,8 @@ countDuet = 0
 logging.info(f'There are {len(all_videos)} videos and {len(verified_user)} verified users.')
 for entry in tqdm.tqdm(all_videos):
     dic = simple_dict(dict(entry))
-    print(dic['duetFromId'], type(dic['duetFromId']), dic['duetFromId'] != None and dic['duetFromId'] != '0')
-    if dic['duetFromId'] != None and dic['duetFromId'] != '0':
+    # print(dic['duetFromId'], type(dic['duetFromId']), dic['duetFromId'] is not None and dic['duetFromId'] != '0')
+    if dic['duetFromId'] != '0':
         countDuet+= 1
     content.append([dic[k] for k in headers])
 logging.info(f'There are {countDuet} dueted videos in total.')
